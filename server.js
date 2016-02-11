19d38ea4eeab67eb1ac64670a8ed4d9a11d6be5c
p1ch_jp <p1ch.jp@gmail.com>
2016-02-12 03:23:14 +0900

    Add express-cluster

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const app = require('./index');
+const cluster = require('express-cluster');
 const http = require('http');
 
 let server = http.createServer(app);
@@ -8,6 +9,8 @@ let server = http.createServer(app);
 let host = process.env.HOST || '127.0.0.1';
 let port = process.env.PORT || 3000;
 
-server.listen(port, host, () => {
-  console.log('Server running on http://' + host + ':' + port);
+cluster(function(worker) {
+  server.listen(port, host, () => {
+    console.log('Server running on http://' + host + ':' + port + ' with pid ' + process.pid);
+  });
 });
