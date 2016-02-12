ef1b6d6424f9be95424780534d0297d8ba287481
p1ch_jp <p1ch.jp@gmail.com>
2016-02-12 22:57:44 +0900

    Add error handling

--- app/express.js
+++ app/express.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// Require node modules
 const bodyParser = require('body-parser');
 const debug = require('debug');
 const express = require('express');
@@ -8,14 +9,33 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const router = require('./router');
 
+// Create express app
 let app = express();
 
+// Use middlewares
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(methodOverride());
 
+// Routing
 app.use('/', router);
+
+// Not found
+app.use(function(req, res, next) {
+  var err = Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Error handling
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500).json({
+    error: {
+      message: err.message
+    }
+  });
+});
 
 module.exports = app;
